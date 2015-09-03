08106106740b4ebd4fdbc772d379408586c0a02b
Peter Andersen <peter@ryanandersen.dk>
2015-09-03 16:43:37 -0400

    added root path handler

--- server.js
+++ server.js
@@ -3,5 +3,9 @@
 /**
  * Module dependencies.
  */
+
+// Add the root project directory to the app module search path: 
+require('app-module-path').addPath(__dirname);
+
 var app = require('./config/lib/app');
 var server = app.start();
