4ed60a50a87c81c9edbeabd4a98fdae6f5ddae52
Peter Andersen <peter@ryanandersen.dk>
2015-09-15 15:38:01 -0400

    content model working with promises

--- modules/content/server/content.model.js
+++ modules/content/server/content.model.js
@@ -4,15 +4,56 @@ var knex = require('config/lib/knex').knex,
   redis = require('config/lib/redis');
 
 var Content = function() {
-//  this.firstName = firstName;
+  this.data = {id: -1};
 };
 
 Content.prototype.get = function(id){
   console.log('get content', id);
 };
 
-Content.prototype.sayHello = function(){
-  console.log('Hello, Im ' + this.firstName);
+Content.prototype.key = function(){
+  var key = 'content:' + this.data.id;
+  return key;
+};
+
+Content.key = function(id){
+  var key = 'content:' + id;
+  return key;
+};
+
+Content.prototype.setCache = function(){
+  var _self = this;
+  return redis.hmset('content:' + this.data.id, _self.data)
+    .then(function(result){
+      return _self;
+    });
+};
+
+Content.prototype.loadFromCache = function(id){
+  var _self = this;
+  return redis.hgetall('content:' + id)
+    .then(function(result){
+      console.log('getCache:', result);
+      _self.data = result;
+      return result;
+    });
+};
+
+Content.prototype.checkCache = function(id){
+  var _self = this;
+  return redis.exists('content:' + id);
+};
+
+Content.prototype.loadFromDB = function(id){
+  var _self = this;
+  return knex('content')
+    .where('id', id)
+    .first()
+    .then(function(result){ 
+      console.log('load from DB: ', result);
+      _self.data = result;
+      return _self;
+    });
 };
 
 Content.findAll = function (callback){
@@ -22,29 +63,32 @@ Content.findAll = function (callback){
     .nodeify(callback);
 };
 
-Content.get = function(id, callback){
-  var key = 'content:' + id;
-  console.log(key);
-  redis.exists(key).then(function(result){
-    console.log('##exists: ', result);
-    if(result === '1'){
-      // cache hit
-      redis.hmget(key, 'url')
-        .then(function(url){
-          console.log('url:', url);
-          return url;
-        });
-    }
-    else{
-      // cache miss
-      return knex('content')
-        .where('id', id).then(function(result){
-          console.log('knex res: ', result);
-          //if()
-          return result;
-        }).nodeify(callback);
-    }
-  }).nodeify(callback);
+
+/*
+ * try to load from from cache otherwise load from DB
+ * and then add to cache
+ */
+Content.get = function(id){
+  var item = new Content();
+
+  return item.checkCache(id)
+    .then(function(result){
+      if(result===1){
+        // load from cache
+        return item.loadFromCache(id);
+      }
+      else{
+        // load from db
+        return item.loadFromDB(id)
+          .then(function(result){
+            // add to cache
+            return item.setCache();
+          })
+          .then(function(result){
+            return item.data;
+          });
+      }
+    });
 };
 
 module.exports = Content;
